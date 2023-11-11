b0cdb8323d317d835f7deb99726d56cb6057b1db
Jason Cox <jcoxey@gmail.com>
2023-11-11 23:13:34 +0000

    Convert head to container

--- head/src/lib/fetcher.ts
+++ head/src/lib/fetcher.ts
@@ -1,6 +1,10 @@
 type Response = {
   status: number,
 }
+
+const config = {
+  endpoint: process.env.NEXT_PUBLIC_CMS_ENDPOINT,
+}
 export type ErrorResponse = Response & {
     type: string,
     title: string,
@@ -35,29 +39,27 @@ export type OkChildrenResponse = Response & {
   items: Child[]
 }
 
-export const getHomeContent =  async (): Promise<string> => {
-    const response = await fetch(`http://localhost:8080/umbraco/delivery/api/v1/content/item/home`)
+export const getHomeContent =  async (): Promise<string> => {    
+    const response = await fetch(`${config.endpoint}/umbraco/delivery/api/v1/content/item/home`)
     return await response.json();    
 }
 
 export async function getChildren<T extends OkChildrenResponse>(path:string) : Promise<T|ErrorResponse> {
-  const url = `http://localhost:8080/umbraco/delivery/api/v1/content?fetch=children:${path}`;
-  console.log(`Url is ${url}`);
+  const url = `${config.endpoint}/umbraco/delivery/api/v1/content?fetch=children:${path}`;
   const response = await fetch(url);
   const result = await response.json();
   result.status = response.status;
-  console.log(`Status is ${result.status}`)
   return result;
 }
 export async function getContent<T extends OkResponse>(path: string): Promise<T|ErrorResponse> {
-    const response = await fetch(`http://localhost:8080/umbraco/delivery/api/v1/content/item/${path}`);
+    const response = await fetch(`${config.endpoint}/umbraco/delivery/api/v1/content/item/${path}`);
     const result = await response.json();
     result.status = response.status;
     return result;  
 }
 
 export const getAllPages = async (): Promise<string> => {
-    const pages = await await fetch("http://localhost:8080/umbraco/delivery/api/v1/content/item/?fetch=descendents")
+    const pages = await await fetch(`${config.endpoint}/umbraco/delivery/api/v1/content/item/?fetch=descendents`)
     return await pages.json();
 }
 
